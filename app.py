78b26d101222fad1c9c214b346136766bea30167
Lenny7120 <ocrank20@gmail.com>
2023-12-21 18:07:35 +0000

    backend application

--- app.py
+++ app.py
@@ -55,3 +55,44 @@ class FireApp:
                 print("Data sent to organization sucessfully.")
             else:
                 print(f"Failed to send data. Server responded with status code {response.status_code}.")
+        except resquests.exceptions.RequestExceptions as e:
+            print(f"An error occured while sending data: {e}")
+            
+    def register_user(self, username, password, emergency_type, user_location):
+        conn = self.create_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO users(username, password, emergency_type, location)
+            VALUES(?, ?, ?, ?)
+            ''', (username, password, emergency_type, user_location))
+        conn.commit()
+        conn.close()
+
+   def handle_registration(self, username, password, emergency_type, user_location):
+       self.register_user(username, password, emergency_type, user_location)
+       self.send_to_organization(username, password, emergency_type, user_location)
+
+
+fire_app = FireApp()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        emergency_type = request.form['emergency_type']
+        user_location = request.form['user_location']
+
+
+        fire_app.handle_registration(username, password, emergency_type, user_location)
+        return redirect(url_for('home'))
+
+    return render_template('register.html', user_location=fire_app.get_user_location())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
